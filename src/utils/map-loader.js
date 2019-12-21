312c86ce4380c08e83524af97a4e172d06e66a3e
kinkin3449 <rxx10086@163.com>
2019-12-21 13:51:53 +0800

    1、修复第一次加载地图无法显示的bug；2、异步添加amapui组件；

--- src/utils/map-loader.js
+++ src/utils/map-loader.js
@@ -6,12 +6,19 @@ export default function MapLoader() {
       var script = document.createElement('script')
       script.type = 'text/javascript'
       script.async = true
-      script.src = 'https://webapi.amap.com/maps?v=1.4.15&key=6b728d458803b1b1b7093e1610def58e'
+      script.src = 'https://webapi.amap.com/maps?v=1.4.15&key=6b728d458803b1b1b7093e1610def58e&callback=initAMap'
       script.onerror = reject
+      var script2 = document.createElement('script')
+      script2.type = 'text/javascript'
+      script2.async = true
+      script2.src = 'https://webapi.amap.com/ui/1.0/main-async.js'
+      script2.onerror = reject
       document.head.appendChild(script)
+      document.head.appendChild(script2)
     }
     window.initAMap = () => {
       resolve(window.AMap)
+      initAMapUI()
     }
   })
 }
